65c9fc92fed4c791525f65f579c127eff7fc6d5b
hefw <hefw@yowits.com>
2019-03-09 23:02:36 +0800

    升级 spring cloud 版本

--- webflux-demo/src/main/java/com/getset/webfluxdemo/handler/TimeHandler.java
+++ webflux-demo/src/main/java/com/getset/webfluxdemo/handler/TimeHandler.java
@@ -6,22 +6,23 @@ import org.springframework.web.reactive.function.server.ServerResponse;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
-import java.text.SimpleDateFormat;
 import java.time.Duration;
-import java.util.Date;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
 
 import static org.springframework.web.reactive.function.server.ServerResponse.ok;
 
 public class TimeHandler {
     public static Mono<ServerResponse> getTime(ServerRequest serverRequest) {
-        return ok().contentType(MediaType.TEXT_PLAIN).body(Mono.just("Now is " + new SimpleDateFormat("HH:mm:ss").format(new Date())), String.class);
+        return ok().contentType(MediaType.TEXT_PLAIN).body(Mono.just("Now is " + LocalTime.now()), String.class);
     }
     public static Mono<ServerResponse> getDate(ServerRequest serverRequest) {
-        return ok().contentType(MediaType.TEXT_PLAIN).body(Mono.just("Today is " + new SimpleDateFormat("yyyy-MM-dd").format(new Date())), String.class);
+        return ok().contentType(MediaType.TEXT_PLAIN).body(Mono.just("Today is " + LocalDate.now()), String.class);
     }
     public static Mono<ServerResponse> sendTimePerSec(ServerRequest serverRequest) {
         return ok().contentType(MediaType.TEXT_EVENT_STREAM).body(
                 Flux.interval(Duration.ofSeconds(1)).
-                        map(l -> new SimpleDateFormat("HH:mm:ss").format(new Date())), String.class);
+                        map(l -> LocalTime.now().format(DateTimeFormatter.ISO_DATE_TIME)), String.class);
     }
 }
